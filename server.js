32b8414f2e0322a7586b2b932019daf68929af3a
Vishal Verma <assistantjunior@earninghandle.com>
2025-01-24 12:55:39 +0530

    cors policy changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // router
 const userRouter = require('./routes/web/user.routes.js');
@@ -13,6 +14,12 @@ const PORT = process.env.PORT || 4000;
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use(cookieParser());
+// Allow requests from your frontend (localhost:3000)
+app.use(cors({
+    origin: 'http://localhost:3000', // Frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // DB connection
 const dbconnect = require('./config/db.js')
